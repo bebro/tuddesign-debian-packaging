72781e9b254a8bd91c7132802d3da5a980d2abe8
Benjamin Brockhaus <tulatex@burning-world.de>
2014-10-30 21:10:43 +0100

    fixed naming-error while moving log-file

--- build_package.sh
+++ build_package.sh
@@ -5,7 +5,7 @@ VERSION_ALT=1.0.20140920
 cd latex-tuddesign-$VERSION_NEU/
 debuild
 cd ..
-mv latex-tuddesign_"$VERSION_NEU"_i386.build build-logs/latex-tuddesign_$VERSION_NEU_i386.build
+mv latex-tuddesign_"$VERSION_NEU"_i386.build build-logs/
 mv latex-tuddesign_$VERSION_NEU.debian.tar.gz hochzuladen/
 mv latex-tuddesign_$VERSION_NEU.dsc hochzuladen/
 mv latex-tuddesign_"$VERSION_NEU"_all.deb hochzuladen/
