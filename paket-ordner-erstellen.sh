dbc59d2fd4fb309e4806af39e80d7d374c210bab
Benjamin Brockhaus <tulatex@burning-world.de>
2014-10-30 20:30:46 +0100

    initial commit, with script to create a folder for the packaging

---
+++ paket-ordner-erstellen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+VERSION_NEU=1.0.20141024
+VERSION_ALT=1.0.20140920
+
+rm -R latex-tuddesign-$VERSION_NEU
+wget https://github.com/bebro/tuddesign/archive/$VERSION_NEU.tar.gz -O latex-tuddesign_$VERSION_NEU.orig.tar.gz
+tar -xf latex-tuddesign_$VERSION_NEU.orig.tar.gz
+mv tuddesign-$VERSION_NEU/ latex-tuddesign-$VERSION_NEU/
+cp -R debian/ latex-tuddesign-$VERSION_NEU/debian/
+cd latex-tuddesign-$VERSION_NEU/
+dquilt push -a
