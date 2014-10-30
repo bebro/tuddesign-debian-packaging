8facba62d9cf9389d332969e5fc0b4602d7781e9
Benjamin Brockhaus <tulatex@burning-world.de>
2014-10-30 21:03:23 +0100

    new script for building the package and cleaning up afterwards

---
+++ build_package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+VERSION_NEU=1.0.20141024
+VERSION_ALT=1.0.20140920
+
+cd latex-tuddesign-$VERSION_NEU/
+#debuild
+cd ..
+mv latex-tuddesign_"$VERSION_NEU"_i386.build build-logs/latex-tuddesign_$VERSION_NEU_i386.build
+mv latex-tuddesign_$VERSION_NEU.debian.tar.gz hochzuladen/
+mv latex-tuddesign_$VERSION_NEU.dsc hochzuladen/
+mv latex-tuddesign_"$VERSION_NEU"_all.deb hochzuladen/
+mv latex-tuddesign_"$VERSION_NEU"_i386.changes hochzuladen/
+mv latex-tuddesign_$VERSION_NEU.orig.tar.gz hochzuladen/
