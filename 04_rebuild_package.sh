559dc194740b3371b784663e4cdc2a8b17f424d3
Benjamin Brockhaus <tulatex@burning-world.de>
2014-12-09 16:07:50 +0100

    added script to rebuild package after manual changes in source-tree

---
+++ 04_rebuild_package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#VERSION_NEU=1.0.20141024
+#VERSION_ALT=1.0.20140920
+
+mv upload/$VERSION_NEU/latex-tuddesign_$VERSION_NEU.orig.tar.gz latex-tuddesign_$VERSION_NEU.orig.tar.gz
+bash ./03_build_package.sh
